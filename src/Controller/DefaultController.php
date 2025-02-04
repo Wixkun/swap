cd05637a724833f6f0311180b98f0a6fb49ecd5e
Matthis <matthisd77@gmail.com>
2025-02-04 12:08:45 +0100

    ADD: all task in homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,28 +4,41 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskFormType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
-    {
+    public function index(
+        Request $request,
+        EntityManagerInterface $em,
+        TaskRepository $taskRepository
+    ): Response {
         $task = new Task();
+
         $form = $this->createForm(TaskFormType::class, $task);
-        $request = Request::createFromGlobals();
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success', 'Tâche créée avec succès !');
+
             return $this->redirectToRoute('app_default');
         }
 
+        $tasks = $taskRepository->findAllPendingFirst();
+
         return $this->render('index.html.twig', [
-            'form' => $form->createView(),
+            'form'  => $form->createView(),
+            'tasks' => $tasks,
         ]);
     }
-
 }
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -16,28 +16,22 @@ class TaskController extends AbstractController
     #[Route('/task', name: 'app_task')]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
-        // Nouveau Task
         $task = new Task();
 
-        // Créer le formulaire
         $form = $this->createForm(TaskFormType::class, $task);
         $form->handleRequest($request);
 
-        // Si soumis + valide
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // -- 1) Récupération du fichier uploadé
             /** @var UploadedFile $imageFile */
             $imageFile = $form->get('imageFile')->getData();
 
-            // -- 2) S'il existe, on le déplace dans /public/uploads/tasks/
             if ($imageFile) {
-                // On crée un nouveau nom unique, ex : "63f5ed2f4cc73.jpg"
                 $newFilename = uniqid().'.'.$imageFile->guessExtension();
 
                 try {
                     $imageFile->move(
-                        $this->getParameter('uploads_directory'), // On verra plus bas ce paramètre
+                        $this->getParameter('uploads_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {
@@ -45,17 +39,15 @@ class TaskController extends AbstractController
                     $this->addFlash('error', 'Impossible d\'uploader l\'image');
                 }
 
-                // On stocke le nom du fichier en BDD
                 $task->setImagePath($newFilename);
             }
 
-            // -- 3) Sauvegarde en BDD
             $em->persist($task);
             $em->flush();
 
             $this->addFlash('success', 'Tâche créée avec succès !');
 
-            return $this->redirectToRoute('app_default'); // Page d'accueil ou autre
+            return $this->redirectToRoute('app_default');
         }
 
         return $this->render('parts/home/taskForm.html.twig', [
